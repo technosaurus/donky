a6eda32b829299ada29deca50f63af4f5c91123f
Matt Hayes <nobomb@gmail.com>
2009-03-08 23:51:23 -0500

    Added autoconf/automake scripts

---
+++ src/main.c
@@ -0,0 +1,29 @@
+/*
+ * This file is part of donky.
+ *
+ * donky is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * donky is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with donky.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Program entry point.
+ * @param argc Argument count
+ * @param argv Array of arguments
+ * @return Exit status
+ */
+int main(int argc, char **argv)
+{
+
+        
+        return 0;
+}
