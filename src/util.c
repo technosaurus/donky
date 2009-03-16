d3f7d7b08aa37014cc150a35bce547cc50d3a9a5
Matt Hayes <nobomb@gmail.com>
2009-03-16 00:16:12 -0500

    Oops, dumbass self forgot to commit the new files!

---
+++ src/util.c
@@ -0,0 +1,43 @@
+/*
+ * This file is part of donky.
+ *
+ * donky is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * donky is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with donky.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+
+#include "util.h"
+
+/**
+ * @brief Trim leading and trailing whitespace from a string.
+ *
+ * @param str Character array pointer
+ *
+ * @return Haxified string
+ */
+char *trim(char *str)
+{
+        int i;
+        
+        for (; isspace(*str); str++) { }
+        
+        for (i = strlen(str) - 1; i >= 0; i--) {
+                if (!isspace(str[i])) {
+                        str[i + 1] = '\0';
+                        break;
+                }
+        }
+
+        return str;
+}
