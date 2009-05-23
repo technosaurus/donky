0747a0b39b03678213ace2c0e8ed467051bfcab1
Matt Hayes <nobomb@gmail.com>
2009-05-23 01:12:00 -0500

    Create m4 dir if it doesn't exist in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #autoreconf --force --install -I config -I m4
 
+# Make m4 directory if it doesn't exist.
+if [ ! -d "m4" ]; then
+	mkdir "m4"
+fi
+
 echo "- libtoolize"
 libtoolize --force
 
